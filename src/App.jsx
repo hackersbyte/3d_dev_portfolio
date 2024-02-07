5f344a3edb3cec9765c924a8e3ab0114cc4884f1
hackersbyte <alexmutonga3@gmail.com>
2024-02-07 16:38:04 +0000

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,36 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { Home, About, Contact, Projects }  from "./pages"
 
-import { Navbar } from "./components"; // remember Footer
-
-
+import { Footer, Navbar } from "./components";
+import { About, Contact, Home, Projects } from "./pages";
 
 const App = () => {
   return (
-    <main className="bg-slate-300/20">
-        <Router>
-            <Navbar />
+    <main className='bg-slate-300/20'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route
+            path='/*'
+            element={
+              <>
             <Routes>
                 <Route path="/" element={<Home/> } />
                 <Route path="/about" element={<About/> } />
                 <Route path="/projects" element={<Projects/> } />
                 <Route path="/contact" element={<Contact/> } />
-            </Routes>
-        </Router>
+                </Routes>
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
 
 
